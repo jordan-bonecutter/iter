114c29c95b782c3d077698a8ef373ffe272d592f
Jordan Bonecutter <jpbonecutter@gmail.com>
2022-11-07 13:59:32 -0800

    add tests

---
+++ map_test.go
@@ -0,0 +1,39 @@
+package iter
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestMap(t *testing.T) {
+	m := IterMap[string, int](map[string]int{
+		"HELLO": 1, "world": 2, "MY": 3, "name": 1, "is": 6, "jordan": 7,
+	})
+
+	m2 := MapCollect(Take(Filter[KeyValue[string, int]](m, func(kv KeyValue[string, int]) (include bool) {
+		return strings.ToUpper(kv.K) != kv.K
+	}), 10), func(el KeyValue[string, int]) (k string, v int) {
+		return el.K, el.V
+	})
+	
+	if len(m2) != 4 {
+		t.Errorf("Expected final collected map to have length 4")
+	}
+	
+	expected := map[string]int {
+		"world": 2, "name": 1, "is": 6, "jordan": 7,
+	}
+	for k2, v2 := range m2 {
+		if v, ok := expected[k2]; !ok {
+			t.Errorf("Unexpected key %v found in map", k2)
+		} else if v != v2 {
+			t.Errorf("Unexpected map value at key %v, expected %v but got %v", k2, v, v2)
+		} else {
+			delete(expected, k2)
+		}
+	}
+	
+	for k, v := range expected {
+		t.Errorf("Missing key value pair: (%v, %v)", k, v)
+	}
+}
---
+++ slice_test.go
@@ -0,0 +1,26 @@
+package iter
+
+import (
+	"testing"
+)
+
+func TestSlice(t *testing.T) {
+	s := IterSlice[int]([]int{1, 2, 3, 4, 5})
+	squared := Collect(Map[int, int](s, func(i int) (o int, stop bool) {
+		return i * i, false
+	}))
+	
+	if len(squared) != 5 {
+		t.Errorf("Expected the final collected array to have length == 5, got %v", len(squared))
+	}
+	
+	if cap(squared) != 5 {
+		t.Errorf("Expected the final collected array to have capacity == 5, got %v", cap(squared))
+	}
+	
+	for idx, el := range squared {
+		if el != (idx+1) * (idx+1) {
+			t.Errorf("Expected i'th element to be %v, got %v", el, (idx+1)*(idx+1))
+		}
+	}
+}
